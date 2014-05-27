01ff6300c213da4c52edb08493b19fb5dc4a978f
Adam Hunter <adamhunter@me.com>
2014-05-27 18:07:15 -0400

    configured application for test mode

--- lib/dummy/application.rb
+++ lib/dummy/application.rb
@@ -22,12 +22,21 @@ module Dummy
   end
 
   class Application < ::Rails::Application
-    config.root                       = Dummy.rails_root
-    config.logger                     = Logger.new("/dev/null")
-    config.eager_load                 = Rails.env.production?
-    config.cache_classes              = !Rails.env.development?
-    config.active_support.deprecation = :stderr
-    config.secret_token               = 'http://s3-ec.buzzfed.com/static/enhanced/webdr03/2013/5/25/8/anigif_enhanced-buzz-11857-1369483324-0.gif'
+    config.root                                       = Dummy.rails_root
+    config.eager_load                                 = Rails.env.production?
+    config.cache_classes                              = !Rails.env.development?
+    config.serve_static_assets                        = true
+    config.static_cache_control                       = "public, max-age=3600"
+    config.whiny_nils                                 = true
+    config.consider_all_requests_local                = true
+    config.action_controller.perform_caching          = false
+    config.action_dispatch.show_exceptions            = false
+    config.active_support.deprecation                 = :stderr
+    config.action_mailer.delivery_method              = :test
+    config.action_controller.allow_forgery_protection = false
+    config.active_support.deprecation                 = :stderr
+    config.eager_load                                 = false
+    config.secret_token                               = 'http://s3-ec.buzzfed.com/static/enhanced/webdr03/2013/5/25/8/anigif_enhanced-buzz-11857-1369483324-0.gif'
   end
 end
 
